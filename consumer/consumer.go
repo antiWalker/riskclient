ab0e2daed56cea26a0c46b99e428e3b3424bcf9b
董洪臣 <donghongchen@haojile.net.cn>
2021-05-07 17:01:10 +0800

    结果落库的时候，把服务器的ip写入进去

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -81,7 +81,8 @@ func doConsumer(params string) error {
 	}
 
 	ctx, _ := context.WithCancel(context.Background())
-
+	ip, _ := common.ExternalIP()
+	ctx = context.WithValue(ctx, "Ip", ip)
 	ctx = context.WithValue(ctx, "TraceId", raw.SubOrderId)
 	hit, _ := handlers.DetectHandler(ruleList, data, ctx)
 	//解析结果
@@ -104,11 +105,12 @@ func doConsumer(params string) error {
 
 // InsertToDb 如果一个订单过多条策略，则可以把这个订单下多个命中的策略批量insert。
 func InsertToDb(ctx context.Context, order *models.Order, HitList []handlers.StrategyResult) {
+	ip := ctx.Value("ip").(string)
 	for _, v := range HitList {
 		ruleRes := v.IsHit
 		if ruleRes {
 			//fmt.Println(ruleRes)
-			id, err := models.AddNegativeGrossProfitResult(order, v.Name)
+			id, err := models.AddNegativeGrossProfitResult(order, v.Name, ip)
 			common.SQLLogger.Infof("TraceId : %d ,StrategyResult : %v , AddNegativeGrossProfitResult : id : %v , err : %v", ctx.Value("TraceId"), v, id, err)
 		}
 	}
